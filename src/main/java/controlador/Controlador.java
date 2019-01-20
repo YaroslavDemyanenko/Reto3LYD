010ab6e6b6dc12790cf0bdf73f02ddf7cada1df4
unknown <yaroslav.demyanenko@elorrieta-errekamari.com>
2019-01-20 20:10:35 +0100

    solucion de errores

---
+++ src/main/java/controlador/Controlador.java
@@ -0,0 +1,37 @@
+package controlador;
+
+import java.awt.event.ActionEvent;
+
+
+
+public class Controlador {
+	/**
+	private VentanaPrincipal vista;
+	private Cliente modelo;
+
+	public Controlador(VentanaPrincipal vista, Cliente modelo) {
+		this.vista = vista;
+		this.modelo = modelo;
+		this.vista.panel1.boton.addActionListener(this);
+		this.vista.panel2.boton.addActionListener(this);
+	}
+
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+
+		if (e.getSource() == vista.panel1.boton) {
+			modelo.setNombre(vista.panel1.textNombre.getText());
+			modelo.setApellido(vista.panel1.textApellido.getText());
+			vista.setContentPane(vista.panel2);
+			vista.panel2.textDatos.setText(modelo.getNombre() + "\n" + modelo.getApellido());
+		}
+
+		if (e.getSource() == vista.panel2.boton) {
+			vista.panel1.setVisible(true);
+			vista.panel2.setVisible(false);
+		}
+
+	}
+	**/
+}
---
+++ src/test/java/testClases/testCliente.java
@@ -0,0 +1,13 @@
+package testClases;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testCliente {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
