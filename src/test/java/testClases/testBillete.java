67ee94a12b10995c3ecb3840e118d14097bf771f
IN1DM3B_11 <IN1DM3B_11@H015112.NET>
2019-01-30 12:38:58 +0100

    Test billete metido

---
+++ src/test/java/testClases/testBillete.java
@@ -0,0 +1,46 @@
+package testClases;
+
+import static org.junit.Assert.*;
+
+import java.sql.Date;
+
+import org.junit.Test;
+
+import clases.Billete;
+import clases.Linea;
+import clases.Parada;
+
+public class testBillete {
+
+	int codigoBillete,codAutobusIda,codAutobusVuelta;
+	float precioTrayecto;
+	Date fechaIda, fechaVuelta;
+	Linea linea;
+	Parada paradaInic, paradaFin;
+	
+	Billete testBillete = new Billete();
+	
+	@Test
+	public void testCodigoBillete() {
+		testBillete.setCodigoBillete(codigoBillete);
+		assertEquals(testBillete.getCodigoBillete(),codigoBillete);	
+	}
+	
+	@Test
+	public void testCodAutobusIda() {
+		testBillete.setCodAutobusVuelta(codAutobusIda);
+		assertEquals(testBillete.getCodAutobusVuelta(),codAutobusIda);	
+	}
+
+	@Test
+	public void testCodAutobusVuelta() {
+		testBillete.setCodAutobusVuelta(codAutobusVuelta);
+		assertEquals(testBillete.getCodAutobusVuelta(),codAutobusVuelta);	
+	}
+	
+	@Test
+	public void testPrecioTrayecto() {
+		testBillete.setPrecioTrayecto(precioTrayecto);
+		assertEquals(testBillete.getPrecioTrayecto(),precioTrayecto, 0.005);	
+	}
+}
