23c3d0b4b9815782b96fe23251ccf646faf2aeba
IN1DM3B_16 <IN1DM3B_16@H015112.NET>
2019-01-31 11:38:29 +0100

    metodos actualizados

--- src/main/java/controlador/Metodos.java
+++ src/main/java/controlador/Metodos.java
@@ -4,15 +4,16 @@ import java.sql.ResultSet;
 import java.util.Calendar;
 import java.util.Date;
 
+import javax.swing.JOptionPane;
+
 import clases.Cliente;
 import clases.Modelo;
 import interfaces.Ventana;
 
-
-public class Metodos{
+public class Metodos {
 
 	public Cliente ingresar(String nombre, String apellido, String dni, String sexo, String contrasenia) {
-		
+
 		Cliente cliente = new Cliente();
 		cliente.setNombre(nombre);
 		cliente.setApellido(apellido);
@@ -22,59 +23,70 @@ public class Metodos{
 		return cliente;
 
 	}
-	
-	public void limitarFechasIda(Ventana vis,int numDias) {
-		Date fechaLimite=new Date();
+
+	public void limitarFechasIda(Ventana vis, int numDias) {
+		Date fechaLimite = new Date();
 		vis.panelLineas2.calendarioIda.setDate(new Date());
 		Calendar c = Calendar.getInstance();
 		c.setTime(fechaLimite);
-		c.add(Calendar.DATE,numDias);
-		fechaLimite=c.getTime();
-		vis.panelLineas2.calendarioIda.setSelectableDateRange(new Date(),fechaLimite);
+		c.add(Calendar.DATE, numDias);
+		fechaLimite = c.getTime();
+		vis.panelLineas2.calendarioIda.setSelectableDateRange(new Date(), fechaLimite);
 	}
-	public void limitarFechasVuelta(Ventana vis,int numDias) {
-		Date fechaLimite=vis.panelLineas2.calendarioIda.getDate();
+
+	public void limitarFechasVuelta(Ventana vis, int numDias) {
+		Date fechaLimite = vis.panelLineas2.calendarioIda.getDate();
 		vis.panelLineas2.calendarioVuelta.setDate(fechaLimite);
 		Calendar c = Calendar.getInstance();
 		c.setTime(fechaLimite);
-		c.add(Calendar.DATE,numDias);
-		fechaLimite=c.getTime();
-		vis.panelLineas2.calendarioVuelta.setSelectableDateRange(vis.panelLineas2.calendarioIda.getDate(),fechaLimite);
+		c.add(Calendar.DATE, numDias);
+		fechaLimite = c.getTime();
+		vis.panelLineas2.calendarioVuelta.setSelectableDateRange(vis.panelLineas2.calendarioIda.getDate(), fechaLimite);
 	}
-	
-	public boolean comprobarDNIenBD(Modelo mod, Cliente cliente) {
+
+	public boolean comprobarDNIenBD(Modelo mod, Ventana vis, Cliente cliente) {
 
 		ResultSet rs = null;
 		boolean estaRegistrado = true;
-		
-		/*Comparamos si el DNI insertado consta en la base de datos o no y hay que aÃ±adirlo*/
+
+		/*
+		 * Comparamos si el DNI insertado consta en la base de datos o no y hay que
+		 * añadirlo
+		 */
 		try {
 			String sql = "select DNI from cliente where DNI = " + cliente.getDni();
 			rs = mod.db.hacerPeticion(sql);
-			/* Si ya existe en la base de datos devuelve un true*/
+			/* Si ya existe en la base de datos devuelve un true */
 			if (rs.next()) {
 				estaRegistrado = true;
-			} 
-			/* Si no esta en la base de datos devuelve un false y lo mete en la base de datos  */
-			else estaRegistrado = false;
+			}
+			/*
+			 * Si no esta en la base de datos devuelve un false y lo mete en la base de
+			 * datos
+			 */
+			else
+				estaRegistrado = false;
 			String sql1 = "insert into DNI values (" + estaRegistrado + ")";
-			
+
 		} catch (Exception e) {
-			System.out.println("Error en obtener usuario");
+			JOptionPane.showMessageDialog(vis.panelLogin, "Error en obtener usuario", "Advertencia", 0);
 		}
-		
+
 		return estaRegistrado;
-		
-	}
-	
-	public void registrarEnBD(Modelo mod, Cliente cliente) {
-		/* Registramos todos los parï¿½metros menos el DNI que lo hemos insertado anteriormente*/
-		String sql1 = "insert into Nombre, Apellidos, Sexo, Contraseï¿½a, Fecha_nac values ('" + cliente.getNombre() + "','" + 
-					cliente.getApellido() + "','"+ cliente.getSexo()+ "','"+ cliente.getContrasenia() + "','" + 
-					cliente.getFecha_nac() + ")";
+
 	}
 
-	public void Login(Modelo mod,String dni, String contrasenia) {
+	public void registrarEnBD(Modelo mod, Cliente cliente) {
+		/*
+		 * Registramos todos los parï¿½metros menos el DNI que lo hemos insertado
+		 * anteriormente
+		 */
+		String sql1 = "insert into Nombre, Apellidos, Sexo, Contraseña, Fecha_nac values ('" + cliente.getNombre()
+				+ "','" + cliente.getApellido() + "','" + cliente.getSexo() + "','" + cliente.getContrasenia() + "','"
+				+ cliente.getFecha_nac() + ")";
+	}
+
+	public void Login(Modelo mod,Ventana vis,String dni, String contrasenia, int CantidadPasajeros) {
 		
 		Boolean login = false;
 		String LoginDB = "";
@@ -84,49 +96,74 @@ public class Metodos{
 			ResultSet rs;
 			
 			/*llamamos a la base de datos el DNI y la contraseï¿½a del cliente*/
-			LoginDB = "select DNI,Contraseï¿½a from cliente";
+			LoginDB = "select DNI,Contraseña from cliente";
 
 
 			rs = mod.db.hacerPeticion(LoginDB);
 
 			/*Comparamos los datos de la base de datos con los que ha introducido en el login el cliente*/
 			if (rs.getString("DNI") == dni){
-				/*Este es el caso ï¿½ptimo donde tanto el DNI y la contraseï¿½a existen y corresponden al mismo usuario*/
-				if(rs.getString("Contraseï¿½a")== contrasenia) {
+				/*Este es el caso optimo donde tanto el DNI y la contraseï¿½a existen y corresponden al mismo usuario*/
+				if(rs.getString("Contraseña")== contrasenia) {
 				login = true;
-				}
+				vis.panelLogin.setVisible(false);
+					if(CantidadPasajeros==0) {
+						vis.panelPago.setVisible(true);
+					}
+					else {
+						vis.panelPasajeroExtra.setVisible(true);
+					}
 				/*En este caso el DNI es correcto pero la contraseï¿½a que corresponde a ese usuario es errï¿½nea*/
+				}
 				else {
-					System.out.println("ContraseÃ±a incorrecta");
+					JOptionPane.showMessageDialog(vis.panelLogin, "Contraseña Incorrecta", "Advertencia", 0);
 					login = false;
 				}
+				
 			}
 			/*En este caso el DNI que ha metido no estï¿½ registrado en la BD*/
 			else {
-				System.out.println(" Usuario inexistente");
+				JOptionPane.showMessageDialog(vis.panelLogin, "Usuario inexistente", "Advertencia", 0);
 				login = false;
 			}
 
 		} catch (Exception e) {
-			System.out.println("Error en obtener usuario");
+			JOptionPane.showMessageDialog(vis.panelLogin, "Error en obtener usuario", "Advertencia", 0);
 		}
 
 	}
-	
-	public void PasajeroExtra(Modelo mod, String dni, String nombre, String apellido, String sexo, int CantidadPasajeros) {
-		
-		Cliente pasajeroExtra[]=new Cliente[CantidadPasajeros];		
-		
-		/* Genera clientes extra en base a la cantidad de pasajeros extra*/
-		while (CantidadPasajeros>0) {
-			pasajeroExtra[CantidadPasajeros]=new Cliente (nombre,apellido,dni, sexo);
-			CantidadPasajeros=CantidadPasajeros-1;
+
+	public void PasajeroExtra(Ventana vis, int CantidadPasajeros) {
+
+		Cliente pasajeroExtra[] = new Cliente[CantidadPasajeros];
+
+		/* Genera clientes extra en base a la cantidad de pasajeros extra */
+		while (CantidadPasajeros > 0) {
+			pasajeroExtra[CantidadPasajeros] = new Cliente(vis.panelPasajeroExtra.textFieldNombre.getText(),
+					vis.panelPasajeroExtra.textFieldApellido.getText(), vis.panelPasajeroExtra.textFieldDNI.getText(),
+					vis.panelPasajeroExtra.textFieldSexo.getToolTipText());
+			CantidadPasajeros = CantidadPasajeros - 1;
 		}
-	}		
+	}
+	
+	public float PrecioTrayecto(Modelo mod) {
+		float precioGasolina=(float) 0.80;
+		float consumo=0;
+		String distancia= "";
+		int asiento=0;
+		ResultSet rs;
+		
+		distancia = "select DNI,Contraseña from cliente";
+
+
+		rs = mod.db.hacerPeticion(distancia);
+		
+		float total=0;
+		/*(0,80*consumoBus*distancia)/Nºasiento*/
+		return total;
+	}
 
 }
-
-
 
 /**
  * ---------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -179,11 +216,11 @@ public class Metodos{
  * for (int i = 0, f = 0,fake=1; i < billetesMonedas.length+fake;i++,f++) {
  * //Mira si tiene que pasar a calcular los decimales if (f==9) { i=3; fake=0; }
  * //Calcula los euros if (euros >= billetesMonedas[i] && f < 9) {
- * model.addElement(billetesMonedas[i] + " ï¿½: " + (euros / billetesMonedas[i]));
- * euros = euros % billetesMonedas[i]; } //Calcula los centimos si ha acabado
- * con los euros else if (decimales >= billetesMonedas[i] && f >= 9) {
- * model.addElement((dosDec.format(billetesMonedas[i]/100f)) + " ï¿½: " +
- * (decimales / billetesMonedas[i])); decimales = decimales %
+ * model.addElement(billetesMonedas[i] + " ï¿½: " + (euros /
+ * billetesMonedas[i])); euros = euros % billetesMonedas[i]; } //Calcula los
+ * centimos si ha acabado con los euros else if (decimales >= billetesMonedas[i]
+ * && f >= 9) { model.addElement((dosDec.format(billetesMonedas[i]/100f)) +
+ * " ï¿½: " + (decimales / billetesMonedas[i])); decimales = decimales %
  * billetesMonedas[i]; } } }
  * 
  **/
